a6841d970b80b5f7a27e383d0feda846ded2df7d
20120454 <dat13102k2@gmail.com>
2022-10-22 13:46:20 +0700

    create header

---
+++ Vue/header.js
@@ -0,0 +1,22 @@
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    template: `
+        <div class="alert alert-primary" role="alert">
+            <div class="row">
+                <div class="col-4" id="mssv">20120454</div>
+                <div class="col-4" id="headingTitle">Movies info</div>
+                <div class="col-4 d-flex align-items-end flex-column bd-highlight mb-3 justify-content-between" id="keyAPI">
+                    <div class="p-2 bd-highlight">key API</div>
+                    <div class="form-check form-switch mt-auto p-2 bd-highlight">
+                        <input class="form-check-input" type="checkbox" id="darkmode" name="darkmode">
+                        <label class="form-check-label" for="mySwitch">Dark Mode</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
---
+++ Vue/main.js
@@ -0,0 +1,17 @@
+import voH from "./header.js"
+
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    components: {
+        voH
+    },
+    template: `
+    <div class="row">
+        <voH/>
+    </div>
+    `
+}
